430ccb3a3a166a9e888dc7e3a700e63e77fef5c2
Ravi Ranjan <raviranjan3570@gmail.com>
2019-07-24 20:54:45 +0530

    properly formatted

--- register.php
+++ register.php
@@ -2,21 +2,21 @@
 
 include 'DatabaseConfig.php';
 
- $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+$con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
 
- $pis = $_POST['pis'];
- $email = $_POST['email'];
- $password = $_POST['password'];
+$pis = $_POST['pis'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
- $CheckSQL = " SELECT * FROM salary WHERE EMP_CODE = '$pis' AND EMAIL='$email' ";
+$CheckSQL = " SELECT * FROM salary WHERE EMP_CODE = '$pis' AND EMAIL='$email' ";
  
- $check = mysqli_fetch_array(mysqli_query($con,$CheckSQL));
+$check = mysqli_fetch_array(mysqli_query($con,$CheckSQL));
  
- if(isset($check)){
+if(isset($check)){
 
  echo 'User Already Registered';
 
- }
+}
 
 else{ 
 
@@ -24,16 +24,13 @@ $Sql_Query = " UPDATE salary SET EMAIL = '$email' WHERE EMP_CODE = '$pis' ";
 
 $Sql_Query_Password = " UPDATE salary SET PASSWORD = '$password' WHERE EMP_CODE = '$pis' ";
 
- if(mysqli_query($con,$Sql_Query) && mysqli_query($con,$Sql_Query_Password))
-
-{
+if(mysqli_query($con,$Sql_Query) && mysqli_query($con,$Sql_Query_Password)){
 
  echo 'Registered Successfully';
 
 }
-else
 
-{
+else{
 
  echo 'Something went wrong';
 
